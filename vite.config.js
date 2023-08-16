507b8669c54c230cd833ce8476dc7abe636223db
ivy.ho <ivy.ho@udngroup.com>
2023-08-16 15:47:53 +0800

    test deploy setting

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/vite-vue-tailwind-cart",
+	base: process.env.NODE_ENV === "production" ? "/vite-vue-tailwind-cart/" : "./",
 	plugins: [
 		vue(),
 		eslintPlugin({
